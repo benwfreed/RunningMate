70e885fba7ad486d4d1d2300297acb67f78b533b
Ben <ben@Bens-MacBook.local>
2016-05-04 17:50:26 -0400

    Add login/registration

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
+var mongoose = require('mongoose');
 var mongo = require('mongodb');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var credentials = require('./credentials.js')
+var credentials = require('./credentials.js');
 
 var app = express();
 
@@ -27,16 +28,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(require('cookie-parser')(credentials.cookieSecret));
 app.use(require('express-session')({
+	name: 'session-id',
     secret: credentials.cookieSecret,
     resave: true,
     saveUninitialized: true
 }));
 
+passport.use(new LocalStrategy(function(username, password, done){
+	for (var u in users) {
+		if (username == users[u].username && password == users[u].password){
+			return done(null, users[u]);
+		}
+	}
+	return done(null, false, {message: 'Unable to login'});
+}
+		
+));
+
+app.use(function(req, res, next) {
+    console.log("middleware 1");
+	next();	
+});
+
+app.use(function(req, res, next) {
+	console.log("middleware 2");
+	next();
+})
 
 app.use('/', routes);
 app.use('/users', users);
+
+//mongoose.connect('mongodb://localhost:27017/runningmate');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,75 +1,56 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
+var mongoose = require('mongoose');
 
+Runner = require('./../runner.js');
+
+var url = 'mongodb://localhost:27017/runningmate';
+mongoose.connect(url);
+var db = mongoose.connection;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Running Mate' });
+  //console.log(req.headers);
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('login', {title: 'Running Mate'});
+});
+
+router.post('/login', function(req, res) {
+	console.log(req.body.username);
+	console.log(req.body.password);
+	res.redirect('/');
 });
 
 router.get('/runnerlist', function(req, res) {
-  var MongoClient = mongodb.MongoClient;
   
-  var url = 'mongodb://localhost:27017/runningmate';
-  
-  MongoClient.connect(url, function(err, db) {
-      if (err) {
-          console.log('Cannot connect to server', err);
-      } else {
-          console.log("Connection Established");
-          
-          var collection = db.collection('runners');
-          
-          collection.find({}).toArray(function(err, result) {
-              if (err) {
-                  res.send(err);
-              } else if (result.length) {
-                  res.render('runnerlist', {
-                      "title" : "Running Mate",
-                      "runnerlist": result
-                  });
-              } else {
-                  res.send('No documents found');
-              }
-              
-              db.close();
-          });
-      }
-  });
+	Runner.getRunners(function(err, runners){
+		if(err) {
+			console.log(err);
+		}
+		res.render('runnerlist', {
+			"title": "Running Mate",
+			"runnerlist": runners
+		});
+	});
 });
 
 router.get('/newrunner', function(req, res) {
     res.render('newrunner', {title: 'Add Runner'});
 });
 
-router.post('/addrunner', function(req, res) {
-    var MongoClient = mongodb.MongoClient;
-    
-    var url = 'mongodb://localhost:27017/runningmate';
-    
-    MongoClient.connect(url, function(err, db) {
-        if (err) {
-            console.log('Cannot connect to server', err);
-        } else {
-            console.log('Connected to server');
+router.post('/addrunner', function(req, res) {			
             
-            var collection = db.collection('runners');
-            
-            var runner1 = {runner: req.body.runner, leavingin: req.body.leavingin, 
+	var runner1 = {runner: req.body.runner, leavingin: req.body.leavingin, 
                 city: req.body.city, state: req.body.state, 
-                gender: req.body.gender, distance: req.body.distanced}; 
-            
-            collection.insert([runner1], function(err, result) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.redirect("runnerlist");
-                }
-                db.close();
-            });
-        }
-    });
+                gender: req.body.gender, distance: req.body.distanced};
+				
+	Runner.addRunner(runner1, function() {
+		res.redirect('/runnerlist');
+	});
 });
 
 module.exports = router;
